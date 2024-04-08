933b5b6aa62572322b205b8150afb090b290131e
rwukii <153258501+rwukii@users.noreply.github.com>
2024-04-09 01:11:33 +0700

    CamelCase.cpp

---
+++ Tuần 3/CamelCase.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int camelcase(string s) {
+    int count = 1;
+    for (char c : s)
+    {
+        if (isupper(c))
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+    string s;
+    getline(cin, s);
+    int result = camelcase(s);
+    fout << result << "\n";
+    fout.close();
+
+    return 0;
+}
